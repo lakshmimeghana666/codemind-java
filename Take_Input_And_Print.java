85d8a3ca688d7acfa251a5fabedce7291106632a
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-08-08 20:22:12 +0530

    import java.util.Scanner; public class take{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String a;         a=sc.nextLine();         System.out.printf("%s",a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class take{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a;
+        a=sc.nextLine();
+        System.out.printf("%s",a);
+    }
+}
