b4ca333ebfc269d7f9862c529d4fc99eb39a06e0
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-08-18 07:32:11 +0530

    import java.util.Scanner; public class take{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         if (ch>='A' && ch<='Z')         {             System.out.printf("uppercase alphabet");         }         else if(ch>='a' && ch<='z')         {             System.out.printf("lowercase alphabet");         }         else         {             System.out.printf("not an alphabet");         }     } }

---
+++ Character_is_Uppercase_or_Lowercase_.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class take{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if (ch>='A' && ch<='Z')
+        {
+            System.out.printf("uppercase alphabet");
+        }
+        else if(ch>='a' && ch<='z')
+        {
+            System.out.printf("lowercase alphabet");
+        }
+        else
+        {
+            System.out.printf("not an alphabet");
+        }
+    }
+}
