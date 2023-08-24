417e9429cc81b8bf682058f11f932c0de139123f
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-08-24 22:02:03 +0530

    import java.util.Scanner; public class s{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] a=new int[n];         for(int i=0;i<n;i++)         {             a[i]=sc.nextInt();         }         int s=0,r=0;         for(int i=0;i<n;i++)         {             if(a[i]%2!=0)             {                 s+=a[i];             }         }         System.out.printf("%d",s);     } }

---
+++ Odd_Sum.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class s{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            a[i]=sc.nextInt();
+        }
+        int s=0,r=0;
+        for(int i=0;i<n;i++)
+        {
+            if(a[i]%2!=0)
+            {
+                s+=a[i];
+            }
+        }
+        System.out.printf("%d",s);
+    }
+}
