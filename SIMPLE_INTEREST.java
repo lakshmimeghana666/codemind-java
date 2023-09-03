16440c2e97155b6bebf75ba3b277e3d4bfeb8f7c
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-09-03 12:19:19 +0530

    import java.util.Scanner; public class S {   public static void main(String[] args)   {       Scanner sc=new Scanner(System.in);       int p=sc.nextInt();       int t=sc.nextInt();       int r=sc.nextInt();       int s=(p*t*r)/100;       System.out.printf("%d",s);   } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class S
+{
+  public static void main(String[] args)
+  {
+      Scanner sc=new Scanner(System.in);
+      int p=sc.nextInt();
+      int t=sc.nextInt();
+      int r=sc.nextInt();
+      int s=(p*t*r)/100;
+      System.out.printf("%d",s);
+  }
+}
