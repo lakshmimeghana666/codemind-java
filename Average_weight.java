fbd0ad401f567d3a046e0868c9b2a57456b9aad7
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-10-19 11:13:30 +0530

    import java .util.*; public class dd {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int b1=sc.nextInt();         int b2=sc.nextInt();         int b3=sc.nextInt();         int x=(3*b1-b2-b3);         System.out.printf("%d",x);     } }

--- Average_weight.java
+++ Average_weight.java
@@ -1,13 +1,13 @@
-import java.util.Scanner;
-public class avg{
-    public static void main(String args[])
+import java .util.*;
+public class dd
+{
+    public static void main(String[] args)
     {
-        Scanner sc= new Scanner(System.in);
-        int x,b1,b2,b3;
-        x=sc.nextInt();
-        b1=sc.nextInt();
-        b2=sc.nextInt();
-        b3=3*x-b1-b2;
-        System.out.printf("%d",b3);
+        Scanner sc=new Scanner(System.in);
+        int b1=sc.nextInt();
+        int b2=sc.nextInt();
+        int b3=sc.nextInt();
+        int x=(3*b1-b2-b3);
+        System.out.printf("%d",x);
     }
 }
