98b8d658fb64aedb2719cb4f71107491d50a0a6f
lakshmimeghana666 <119840524+lakshmimeghana666@users.noreply.github.com>
2023-10-19 11:05:42 +0530

    import java.util.Scanner; public class d {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n1=sc.nextInt();         int n2=sc.nextInt();         double n=(float)(n1+n2)/2.0;         System.out.printf("Average of %d and %d is: %.2f",n1,n2,n);     } }

--- Average_of_two_numbers.java
+++ Average_of_two_numbers.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
-public class average
+public class d
 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
-        int a,b;
-        double res;
-        a=sc.nextInt();
-        b=sc.nextInt();
-        res=(a+b)/2.0;
-        System.out.printf("Average of %d and %d is: %.2f",a,b,res);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        double n=(float)(n1+n2)/2.0;
+        System.out.printf("Average of %d and %d is: %.2f",n1,n2,n);
     }
 }
